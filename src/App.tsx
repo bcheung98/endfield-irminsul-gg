f36e5daafbb6ed35c84ac4c24f6ea06edea22baf
Brian <briantcheung@gmail.com>
2025-02-06 16:39:12 -0800

    App updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { useEffect } from "react";
+import { BrowserRouter } from "react-router";
 import "App.css";
 
+// Component imports
+import RouteConfig from "components/nav/RouteConfig";
+
+// MUI imports
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+// Helper imports
+import { fetchCharacters } from "rtk/fetchData";
+import { useAppDispatch, useAppSelector } from "helpers/hooks";
+import { selectTheme, setTheme } from "reducers/settings";
+import { getTheme } from "themes/theme";
+
 function App() {
-    return <></>;
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCharacters());
+    }, []);
+
+    const theme = useAppSelector(selectTheme);
+
+    useEffect(() => {
+        dispatch(setTheme(theme));
+    }, [theme]);
+
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={getTheme(theme)}>
+                <CssBaseline />
+                <RouteConfig />
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
