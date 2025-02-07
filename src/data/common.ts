fb2572f8a7b23d4af6fb646a79ea0088307c3313
Brian <briantcheung@gmail.com>
2025-02-06 16:37:11 -0800

    Added data

---
+++ src/data/common.ts
@@ -0,0 +1,25 @@
+export const elements = [
+    "Physical",
+    "Cryo",
+    "Electric",
+    "Heat",
+    "Nature",
+] as const;
+
+export const classes = [
+    "Guard",
+    "Defender",
+    "Caster",
+    "Supporter",
+    "Specialist",
+] as const;
+
+export const weapons = [
+    "Sword",
+    "Great Sword",
+    "Polearm",
+    "Guns",
+    "Orbiter",
+] as const;
+
+export const rarities = [6, 5, 4, 3, 2, 1] as const;
---
+++ src/data/versions.ts
@@ -0,0 +1,4 @@
+// MAKE SURE TO CHANGE THIS EVERY UPDATE!
+export const updates = [
+    { version: "CBT2", name: "Second Beta Test" },
+];
